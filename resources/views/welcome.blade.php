17a52e5e5c8941b65d6ef1ace1a3d21912ca6e70
mrashidcit <mrashidcit@gmail.com>
2017-07-20 10:16:08 +0500

    first commit giving error on signUp form Submition

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcom!
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sign Up</h3>
+            <form action="{{ route('signup') }}" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input type="text" name="email" id="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input type="text" name="first_name" id="first_name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your E-mail</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                {{ csrf_field() }}
+
+            </form>
+
+        </div>
+
+        <div class="col-md-6">
+            <h3>Sign In</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input type="text" name="email" id="email" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Your E-mail</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+            </form>
+
+        </div>
+    </div>
+@endsection
